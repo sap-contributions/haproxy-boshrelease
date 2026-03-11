f1d2cd0302690bace8bc7e35ce5d6d40ce24163f
Mike Yeromko <mykhailo.yeromko@sap.com>
2026-03-11 11:10:11 +0100

    CFN-6544: cgroupns mode

--- ci/scripts/start-bosh.sh
+++ ci/scripts/start-bosh.sh
@@ -246,6 +246,8 @@ EOF
         -o docker/cpi.yml \
         -o jumpbox-user.yml \
         -o /usr/local/local-releases.yml \
+        -o "$ops_files_dir/bosh-cgroup.yml" \
+        -o "$ops_files_dir/bosh-dns.yml" \
         -o "$ops_files_dir/bosh-scaled-out.yml" \
         -o "$ops_files_dir/bosh-watch-time.yml" \
         -v director_name=docker \

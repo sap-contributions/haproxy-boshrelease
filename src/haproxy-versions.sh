782e8f504840262e8969158a9071a84bed553900
Clemens Hoffmann <clemens.hoffmann@sap.com>
2026-09-10 16:05:38 +0200

    Fix versions

--- src/haproxy-versions.sh
+++ src/haproxy-versions.sh
@@ -2,10 +2,10 @@
 # Shared version definitions for all HAProxy packages.
 # Sourced by packaging scripts in packages/ and packages-multi/.
 
-HAPROXY_VERSION=3.2.19  # https://www.haproxy.org/download/3.2/src/haproxy-3.2.19.tar.gz
-LUA_VERSION=5.4.8  # https://www.lua.org/ftp/lua-5.4.8.tar.gz
+HAPROXY_VERSION=3.2.23  # https://www.haproxy.org/download/3.2/src/haproxy-3.2.23.tar.gz
+LUA_VERSION=5.4.9  # https://www.lua.org/ftp/lua-5.4.9.tar.gz
 PCRE_VERSION=10.47  # https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.47/pcre2-10.47.tar.gz
-SOCAT_VERSION=1.8.1.1  # http://www.dest-unreach.org/socat/download/socat-1.8.1.1.tar.gz
+SOCAT_VERSION=1.8.1.3  # http://www.dest-unreach.org/socat/download/socat-1.8.1.3.tar.gz
 HATOP_VERSION=0.8.2  # https://github.com/jhunt/hatop/releases/download/v0.8.2/hatop
 AWS_LC_VERSION=5.8.0  # https://github.com/aws/aws-lc/archive/refs/tags/v5.8.0.tar.gz
 AWS_LC_FIPS_VERSION=4.0.0  # https://github.com/aws/aws-lc/archive/refs/tags/v4.0.0.tar.gz

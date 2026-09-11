8555a7bae2447259f8552d65434f50329e179a1d
Clemens Hoffmann <clemens.hoffmann@sap.com>
2026-09-11 15:16:40 +0200

    Sync PCRE_VERSION to 10.48 after rebase onto master
    master bumped the pcre2 blob to 10.48; align src/haproxy-versions.sh so the
    packaging glob resolves the committed blob.

--- src/haproxy-versions.sh
+++ src/haproxy-versions.sh
@@ -9,6 +9,6 @@
 
 HAPROXY_VERSION=3.2.23  # https://www.haproxy.org/download/3.2/src/haproxy-3.2.23.tar.gz
 LUA_VERSION=5.4.9  # https://www.lua.org/ftp/lua-5.4.9.tar.gz
-PCRE_VERSION=10.47  # https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.47/pcre2-10.47.tar.gz
+PCRE_VERSION=10.48  # https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.48/pcre2-10.48.tar.gz
 SOCAT_VERSION=1.8.1.3  # http://www.dest-unreach.org/socat/download/socat-1.8.1.3.tar.gz
 HATOP_VERSION=0.8.2  # https://github.com/jhunt/hatop/releases/download/v0.8.2/hatop
